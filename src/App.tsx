e781b088fbc77409c81e4effea91c18cf542031c
ksm <eltlwjs3@gmail.com>
2022-03-12 20:49:48 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { hoursSelector, hourState, minState } from './atmos';
+
+
 
 function App() {
+  //useRecoilState -> atom 값과 atom을 수정 할 수 있는 함수를 전달
+  const [minutes, setMinutes] = useRecoilState(minState);
+  //const hours = useRecoilValue(hoursSelector);
+  const [hours, setHours] = useRecoilState(hoursSelector);
+
+  const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+    //sethours((+event.currentTarget.value) / 60);
+  };
+  
+  const onHoursChange  = (event:React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="분"
+      ></input>
+      <input onChange={onHoursChange} value={hours} type="number" placeholder="시간"></input>
     </div>
   );
 }
---
+++ src/atmos.tsx
@@ -0,0 +1,25 @@
+import {atom, selector} from "recoil";
+
+export const minState = atom({
+    key:"minutes",
+    default : 0
+})
+
+export const hourState = atom({
+    key:"hours",
+    default : 0
+})
+
+export const hoursSelector = selector<number>({
+   key:"hours",
+   get: ({get}) => {
+       const val = get(minState);
+
+       return val / 60;
+   },
+   set : ({set},newVal) => {
+    console.log(newVal);
+    const val = Number(newVal) * 60;
+    set(minState, val)
+   }
+});
